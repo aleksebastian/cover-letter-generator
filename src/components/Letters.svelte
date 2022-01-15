a57b3b1da9aa1d1f87f72713ee9fdd3cca403d70
Alek <aleksebastian@gmail.com>
2022-01-15 15:08:39 -0600

    Reworded and added expand and close logic

--- src/components/Letters.svelte
+++ src/components/Letters.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
+	import { slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+
+	let hidden = false;
+	let transform = '-180deg';
+
+	const handleClick = () => {
+		if (hidden) {
+			transform = '-180deg';
+		} else {
+			transform = '0deg';
+		}
+		hidden = !hidden;
+	};
 </script>
 
 <div class="container">
 	<div class="main">
-		<h2 class="section-text">Letters</h2>
-		<button class="button">Get letters</button>
+		<div class="header">
+			<h2 on:click={handleClick} class="section-text">Cover letters</h2>
+			<div class="arrow-container">
+				<span on:click={handleClick} class="arrow" style="transform: rotateZ({transform})" />
+			</div>
+		</div>
+		{#if !hidden}
+			<button transition:slide={{ delay: 250, duration: 300, easing: quintOut }} class="button"
+				>Get cover letters</button
+			>
+		{/if}
 	</div>
 </div>
 
@@ -19,8 +42,29 @@
 		max-height: 800px;
 	}
 
+	.header {
+		display: flex;
+		justify-content: space-between;
+	}
+
 	.section-text {
 		padding: 1rem;
+		cursor: pointer;
+	}
+
+	.arrow-container {
+		transform: rotate(-45deg);
+	}
+
+	.arrow {
+		display: inline-block;
+		margin: 1.7rem 1rem 0 0;
+		width: 15px;
+		height: 15px;
+		border-top: 3px solid white;
+		border-right: 3px solid white;
+		cursor: pointer;
+		transition: all 300ms;
 	}
 
 	button {
