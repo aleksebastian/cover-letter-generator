d6102b61b203758b2b01f89152efea958c1ea056
Alek <aleksebastian@gmail.com>
2022-01-15 15:08:54 -0600

    Created modal HOC component

---
+++ src/components/Modal.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	import type { SvelteComponent } from 'svelte';
+	import type { SvelteComponentDev, SvelteComponentTyped } from 'svelte/internal';
+
+	export let component: SvelteComponent;
+	export let closeModal;
+</script>
+
+<div transition:fade={{ delay: 150, duration: 150 }} class="container">
+	<div>
+		<p class="close" on:click={closeModal}>&#9587;</p>
+		<svelte:component this={component} />
+	</div>
+</div>
+
+<style>
+	.container {
+		/* modal window center */
+		position: fixed;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		background-color: #393e46;
+		color: white;
+		padding: 2rem;
+		border-radius: 20px;
+	}
+
+	/* modal content center */
+	.content {
+		display: flex;
+		justify-content: center; /* horizontal */
+		align-items: center; /* vertical */
+	}
+
+	.close {
+		text-align: right;
+		cursor: pointer;
+		/* padding: 0.5rem; */
+		color: white;
+	}
+</style>
