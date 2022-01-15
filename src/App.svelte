6b4274daf385b858c88ddad2aafd87a72af1e021
Alek <aleksebastian@gmail.com>
2022-01-15 00:31:25 -0600

    Added and imported stringToHash func and letters component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import Nav from './components/Nav.svelte';
 	import Editor from './components/Editor.svelte';
+	import Letters from './components/Letters.svelte';
 	import Fields from './components/Fields.svelte';
+
+	import { stringToHash } from './utils';
 </script>
 
 <Nav />
 <div class="container">
 	<Editor />
-	<Fields />
+	<div class="second-column">
+		<Letters />
+		<Fields />
+	</div>
 </div>
 
 <style>
@@ -15,6 +21,14 @@
 		display: grid;
 		grid-template-columns: repeat(8, 1fr);
 		padding: 2rem;
+		gap: 2rem;
+	}
+
+	.second-column {
+		grid-column-start: 6;
+		grid-column-end: 9;
+		display: flex;
+		flex-direction: column;
 		gap: 2rem;
 	}
 </style>
