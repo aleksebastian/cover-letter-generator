a19cca26556a6f8815f4b562ffa2c33e6a5c6d82
Alek <aleksebastian@gmail.com>
2022-01-15 00:31:38 -0600

    Letters component WIP

---
+++ src/components/Letters.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+	<div class="main">
+		<h2 class="section-text">Letters</h2>
+		<button class="button">Get letters</button>
+	</div>
+</div>
+
+<style>
+	/* .container {
+		grid-column-start: 6;
+		grid-column-end: 9;
+	} */
+
+	.main {
+		display: flex;
+		flex-direction: column;
+		padding: 0 1rem 1rem 1rem;
+		background-color: #222831;
+		border-radius: 20px;
+		color: white;
+		max-height: 800px;
+	}
+
+	.section-text {
+		padding: 1rem;
+	}
+
+	button {
+		width: 90%;
+		margin: 0.6rem auto;
+	}
+</style>
