780408396df6d7d12113defec8569171b481d701
Alek <aleksebastian@gmail.com>
2022-01-15 22:51:27 -0600

    Created about and instruction sections

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,54 @@
-<div class="container">This is the about modal</div>
+<div class="container">
+	<p class="header">About this Cover Letter Generator</p>
+	<p class="subheader">The motivation to build this was twofold.</p>
+	<ol>
+		<li>
+			The one time I left a company's name in one of my cover letter templates, used that template,
+			and sent the cover letter with one occurrence of the wrong company name, I knew I could use
+			something like this.
+		</li>
+		<li>I love building things in Svelte.</li>
+	</ol>
+	<p class="text">
+		Feel free to checkout the code, fork and clone, etc by clicking <a
+			href="https://github.com/aleksebastian/cover-letter-generator"
+			target="_blank"
+			rel="“noopener”">here</a
+		>.
+	</p>
+	<p class="text">
+		Interested in hiring me to build some kick-ass features for your company? Get in touch and check
+		out my other skills by clicking <a
+			href="https://aleksebastian.dev/contact"
+			target="_blank"
+			rel="“noopener”">here</a
+		>.
+	</p>
+</div>
 
 <style>
 	.container {
-		width: 400px;
-		height: 400px;
-		background: salmon;
+		max-width: 450px;
+	}
+
+	.header {
+		font-size: 1.5rem;
+	}
+
+	.subheader {
+		font-weight: 300;
+	}
+	li {
+		font-weight: 300;
+		padding: 0.4rem;
+	}
+
+	a {
+		color: skyblue;
+	}
+
+	.text {
+		font-weight: 400;
+		padding: 0.5rem;
 	}
 </style>
--- src/components/Instructions.svelte
+++ src/components/Instructions.svelte
@@ -1,1 +1,34 @@
-<div>This is the instructions modal</div>
+<p class="header">Cover Letter Generator - How to use</p>
+<ol>
+	<li>
+		Type or paste your letter text into the text editor, wrapping the words you want to replace in
+		curly braces. <p>
+			E.g. I am thrilled at the prospect of being a {`{role name}`} at {`{company name}`}.
+		</p>
+	</li>
+	<li>
+		Type the replacement word in the textbox that appears under each word under Fields to replace.
+	</li>
+	<li>
+		Once you're done formatting your letter and adding replacement words, use the buttons below the
+		editor to either download the rendered letter as a PDF or copy the rendered text into your
+		clipboard to paste anywhere.
+	</li>
+	<li>
+		For now, you should save your dynamic cover letter by copying the formatted letter from the
+		editor (with the curly braces) and pasting it elsewhere to save it. Paste the text back into the
+		editor next time you want to render a new cover letter from your template. This way, you format
+		your letter once and only fill in the replacement text words next time.
+	</li>
+</ol>
+
+<style>
+	.header {
+		font-size: 1.5rem;
+	}
+
+	li {
+		font-weight: 300;
+		padding: 0.4rem;
+	}
+</style>
