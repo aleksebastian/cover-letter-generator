31402248b30cd2d24f22e68317e95cb43f919376
Alek <aleksebastian@gmail.com>
2022-01-15 22:53:53 -0600

    Added media queries and close modal functionality

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,37 +1,41 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
 	import type { SvelteComponent } from 'svelte';
-	import type { SvelteComponentDev, SvelteComponentTyped } from 'svelte/internal';
+	import { fade } from 'svelte/transition';
+	import { clickOutside } from '../utils';
 
-	export let component: SvelteComponent;
-	export let closeModal;
+	export let component: any;
+	export let componentProp: any;
+	export let closeModal: any;
 </script>
 
-<div transition:fade={{ delay: 150, duration: 150 }} class="container">
+<div
+	transition:fade={{ delay: 150, duration: 150 }}
+	id="modal"
+	class="container"
+	use:clickOutside
+	on:outsideclick={closeModal}
+>
 	<div>
 		<p class="close" on:click={closeModal}>&#9587;</p>
-		<svelte:component this={component} />
+		<svelte:component this={component} {componentProp} />
 	</div>
 </div>
 
 <style>
 	.container {
 		/* modal window center */
+		z-index: 20;
 		position: fixed;
 		left: 50%;
-		top: 50%;
+		top: 40%;
 		transform: translate(-50%, -50%);
-		background-color: #393e46;
+		background-color: #222831;
 		color: white;
 		padding: 2rem;
 		border-radius: 20px;
-	}
-
-	/* modal content center */
-	.content {
-		display: flex;
-		justify-content: center; /* horizontal */
-		align-items: center; /* vertical */
+		max-height: 60vh;
+		overflow: scroll;
+		box-shadow: 0 0 0 1000px rgba(0, 0, 0, 0.658);
 	}
 
 	.close {
@@ -39,5 +43,25 @@
 		cursor: pointer;
 		/* padding: 0.5rem; */
 		color: white;
+	}
+
+	@media screen and (max-width: 1280px) {
+		.container {
+			/* modal window center */
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			max-height: 85vh;
+		}
+	}
+
+	@media screen and (max-width: 768px) {
+		.container {
+			/* modal window center */
+			top: 17%;
+			left: 10%;
+			transform: translate(-5%, -5%);
+			max-height: 85vh;
+		}
 	}
 </style>
