4dd0a7d703a6b0b6bb1c8e073677f9e81a47dcfd
Alek <aleksebastian@gmail.com>
2022-01-15 22:55:45 -0600

    Created warning component to prevent letter overwriting

---
+++ src/components/Warning.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let componentProp: any;
+</script>
+
+<div class="container">
+	<p>This will overwrite your current letter. Are you sure?</p>
+	<p />
+	<button on:click={componentProp}>Yes, open letter</button>
+</div>
+
+<style>
+	.container {
+		width: 300px;
+		padding: 1rem;
+	}
+
+	button {
+		margin: 1rem 0;
+	}
+</style>
